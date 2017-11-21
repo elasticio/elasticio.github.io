e4124fcc15047c9223fcdd9628de23c43d9876bb
Igor Drobiazko <igor@elastic.io>
2017-11-21 15:43:06 +0100

    Search css

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,18 @@ var search = instantsearch({
     appId: 'V2486N1OKP',
     apiKey: '8b8391d5265b293410fca650e3cd2b9e',
     indexName: 'test_elasticio',
-    urlSync: true
+    urlSync: false,
+    searchFunction: function(helper) {
+        var searchResults = $('.search-results');
+        if (helper.state.query === '') {
+            console.log('Hiding search results');
+            searchResults.hide();
+            return;
+        }
+        helper.search();
+        searchResults.show();
+        console.log('Showing search results');
+    }
 });
 
 search.addWidget(
