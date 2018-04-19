47be8e1d955c5d5a047ab0ff42749aef7d60fe05
Igor Drobiazko <igor@elastic.io>
2018-04-19 22:38:24 +0200

    Applied Denis's design

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,3 +1,37 @@
 $(function() {
+    var $header = $('header'),
+        $search = $('.search input'),
+        burgerDelay = 0;
+
+    $search.on('focus', function() {
+        $header.addClass('header_search');
+    });
+
+    $search.on('blur', function() {
+        $header.removeClass('header_search');
+    });
+
+    $(document).on('mousemove', function(e) {
+        if (burgerDelay) {
+            if ($(e.target).is('.burger .big-menu *')) {
+                $('body').addClass('burger_open');
+            } else {
+                $('body').removeClass('burger_open');
+            }
+        }
+    });
+
+    $(document).click(function(m) {
+        if ($(m.target).is('.burger, .burger > *')) {
+            $('body').addClass('burger_open');
+            burgerDelay = 0;
+            setTimeout(function() {
+                burgerDelay = 1
+            }, 1000)
+        } else {
+            $('body').removeClass('burger_open');
+        }
+    })
+
 
 });
