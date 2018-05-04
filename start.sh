36b8a29c9ba72f722f4ec79bc43d91f110d8d89a
Renat Zubairov <renat.zubairov@elastic.io>
2018-05-04 15:00:56 +0200

    Small fix to make it run on Mac OS

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll --volume="$(pwd)/vendor/bundle:/usr/local/bundle" -it -p $(docker-machine ip `docker-machine active`):4000:4000 jekyll/jekyll jekyll serve
